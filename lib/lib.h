77de7cb80e6de17942e8ea7df753d050a6d7c32d
TonegpengCheng <985945435@qq.com>
2020-12-30 20:50:39 -0800

    add rectange func and show bmp

--- lib/lib.h
+++ lib/lib.h
@@ -3,6 +3,13 @@
 
 #define LIB_VERSION "1.0"
 
+#define RED 0x00FF0000
+#define GREEN 0x0000FF00
+#define BLUE  0x000000FF
+#define BLACK 0x00000000
+#define WHITE 0x00FFFFFF
+
+int lcd_show_color(unsigned int color);
 // Func
 
 #endif // LIB
